9fd31e02a93698832e9ca6e1b21638fa92b77815
AndersonNesi <nesi.anderson@outlook.com>
2019-05-31 22:31:34 -0300

    busca por hora

--- app/controllers/busca_clima_controller.rb
+++ app/controllers/busca_clima_controller.rb
@@ -7,10 +7,21 @@ class BuscaClimaController < ApplicationController
       render json: Comunicacao_weather.new.buscar(clima_params[:cidade]), status: :ok
     end
   
-    private
+    
   
+    def proximas_horas
+        render json: ComunicacaoHora.new.proximas_horas(clima_params[:cidade]), status: :ok
+    end
+
+    # def proximos_dias
+    
+    # end
+
+    private
     def clima_params
       params.permit(:cidade)
     end
+
+  
 
 end
---
+++ app/services/comunicacao_hora.rb
@@ -0,0 +1,12 @@
+class ComunicacaoHora
+
+    
+   
+    def proximas_horas(cidade) 
+        @cidade = cidade
+        url = "http://api.openweathermap.org/data/2.5/forecast/hourly?&APPID=7f6645212272a6db61cff3b4dd8cf512&q=#{@cidade},br"
+        retorno = JSON.parse(Net::HTTP.get(URI(url)))
+       
+        
+    end
+end
