88f8d473c1f09b9e08c5c7b084a0f047eff25f34
AndersonNesi <nesi.anderson@outlook.com>
2019-05-31 22:33:05 -0300

    atualizado pois estava com erro

--- app/services/comunicacao_hora.rb
+++ app/services/comunicacao_hora.rb
@@ -4,7 +4,7 @@ class ComunicacaoHora
    
     def proximas_horas(cidade) 
         @cidade = cidade
-        url = "http://api.openweathermap.org/data/2.5/forecast/hourly?&APPID=7f6645212272a6db61cff3b4dd8cf512&q=#{@cidade},br"
+        url = "http://api.openweathermap.org/data/2.5/forecast?&APPID=7f6645212272a6db61cff3b4dd8cf512&q=#{@cidade},br"
         retorno = JSON.parse(Net::HTTP.get(URI(url)))
        
         
